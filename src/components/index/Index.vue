47870738fcfcc0ae5fe632879d34384862cf4fb2
sky-ADMIN <546485299@qq.com>
2017-02-23 17:19:57 +0800

    update

---
+++ src/components/index/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index-container">
+123
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  .index-container {
+    background: url('../../assets/bg.jpg');
+    height: 100%;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello'
+import Index from 'components/index/Index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // },
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+      name: 'Index',
+      component: Index
+    },
   ]
 })
