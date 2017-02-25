dfc7e43e0ce5a406495cc0972be3990a95e300ce
sky-ADMIN <546485299@qq.com>
2017-02-25 17:26:03 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,6 +10,7 @@ import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
 
+Vue.use(Vuex)
 Vue.use(ElementUI)
 Vue.use(VueResource)
 Vue.http.options.emulateJSON = true;
