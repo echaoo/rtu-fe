3d5c5226e766eb1c7c97985877cfd0f37f74fcf6
sky-ADMIN <546485299@qq.com>
2017-02-26 14:51:39 +0800

    add 404 page

---
+++ src/components/404/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="not-found-container">
+    <h1>Ops!页面被饥饿的前端工程师吃掉了。</h1>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  .not-found-container {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from 'components/index/Index'
 import Main from 'components/main/Main'
+import NotFound from 'components/404/NotFound'
 
 Vue.use(Router)
 
@@ -21,6 +22,11 @@ export default new Router({
       path: '/main',
       name: 'main',
       component: Main
+    },
+    {
+      path: '*',
+      name: '404',
+      component: NotFound
     }
   ]
 })
