254be34fcb2b959a5328541418143b9b43498b23
echaoo <zmk181@gmail.com>
2017-02-27 20:08:43 +0800

    添加map

--- src/components/main/map/Map.vue
+++ src/components/main/map/Map.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
-    123
+    <div class="map">
+      <img :src="mapPath" alt="ass">
+    </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import API from '../../../config/request'
+
+  export default {
+    data () {
+      return {
+        mapPath: ''
+      }
+    },
+    created () {
+      let that = this;
+      this.$http.get(API.block).then(
+        function (res) {
+          if (res.data.status === '0') {
+            that.mapPath = res.data.data[0].MapPath;
+            that.mapPath = 'http://' + that.mapPath;
+          }
+        })
+    }
+  }
 </script>
 
 <style lang="less" rel="stylesheet/less">
+  .map {
 
+  }
 </style>
