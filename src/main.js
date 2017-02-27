76314b5d93abfd63c39a19a9d1c3f1433a20cf4e
sky-ADMIN <546485299@qq.com>
2017-02-27 19:50:39 +0800

    remove 别人封装的echart

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import 'element-ui/lib/theme-default/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
 import 'animate.css/animate.css'
-import ECharts from 'vue-echarts/components/ECharts.vue'
-import 'vue-echarts/dist/vue-echarts'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
@@ -16,7 +14,6 @@ import store from './store'
 
 Vue.use(ElementUI)
 Vue.use(VueResource)
-Vue.use(ECharts)
 Vue.http.options.emulateJSON = true;
 Vue.http.options.credentials = true;
 
