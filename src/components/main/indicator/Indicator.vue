5effc0619cdd32a03db5d797d179887b7bf1638d
echaoo <zmk181@gmail.com>
2017-02-27 13:23:35 +0800

    添加施工图

---
+++ src/components/main/indicator/Indicator.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    施工图
+    <!--<div class="wrapper animated fadeInRight">-->
+      <!--<div class="row">-->
+        <!--<div class="col-lg-6">-->
+          <!--<div class="ibox float-e-margins">-->
+            <!--<div class="ibox-title">-->
+              <!--<h5>Simple line chart-->
+              <!--</h5>-->
+            <!--</div>-->
+            <!--<div class="ibox-content">-->
+              <!--<div id="ct-chart1" class="ct-perfect-fourth"></div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-6">-->
+          <!--<div class="ibox float-e-margins">-->
+            <!--<div class="ibox-title">-->
+              <!--<h5>Line scatter diagram</h5>-->
+            <!--</div>-->
+            <!--<div class="ibox-content">-->
+              <!--<div id="ct-chart2" class="ct-perfect-fourth"></div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</div>-->
+      <!--<div class="row">-->
+        <!--<div class="col-lg-6">-->
+          <!--<div class="ibox float-e-margins">-->
+            <!--<div class="ibox-title">-->
+              <!--<h5>Stacked bar chart</h5>-->
+            <!--</div>-->
+            <!--<div class="ibox-content">-->
+              <!--<div id="ct-chart3" class="ct-perfect-fourth"></div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-6">-->
+          <!--<div class="ibox float-e-margins">-->
+            <!--<div class="ibox-title">-->
+              <!--<h5>Horizontal bar chart </h5>-->
+            <!--</div>-->
+            <!--<div class="ibox-content">-->
+              <!--<div id="ct-chart4" class="ct-perfect-fourth"></div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</div>-->
+      <!--<div class="row">-->
+        <!--<div class="col-lg-6">-->
+          <!--<div class="ibox float-e-margins">-->
+            <!--<div class="ibox-title">-->
+              <!--<h5>Simple pie chart</h5>-->
+            <!--</div>-->
+            <!--<div class="ibox-content">-->
+              <!--<div id="ct-chart5" class="ct-perfect-fourth"></div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-6">-->
+          <!--<div class="ibox float-e-margins">-->
+            <!--<div class="ibox-title">-->
+              <!--<h5>Gauge chart </h5>-->
+            <!--</div>-->
+            <!--<div class="ibox-content">-->
+              <!--<div id="ct-chart6" class="ct-perfect-fourth"></div>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</div>-->
+    <!--</div>-->
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="less" rel="stylesheet/less">
+  .wrapper {
+    padding: 20px 10px 40px;
+  }
+</style>
