618522101b3a14bf4dd9236607c26c86959966e2
echaoo <zmk181@gmail.com>
2017-02-27 18:11:01 +0800

    尝试echart

---
+++ src/components/chart/LineChart.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="main"></div>
+</template>
+
+<script>
+  import * as echarts from "echarts";
+  export default {
+    created() {
+      var myChart = echarts.init(document.getElementById('main'));
+
+
+    },
+    methods: {
+
+    },
+    components: {
+    }
+  }
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+  #main {
+    width: 100%;
+    height: 100%;
+  }
+</style>
