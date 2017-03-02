c1490e74001c510982e5a5611902d097b51ae958
echaoo <zmk181@gmail.com>
2017-03-02 17:00:58 +0800

    分页初稿

--- src/components/main/warnlog/WarnLog.vue
+++ src/components/main/warnlog/WarnLog.vue
@@ -4,17 +4,22 @@
       <div class="col-lg-12">
         <div class="ibox float-e-margins">
           <div class="ibox-title">
-            <h5>Basic Data Tables example with responsive plugin</h5>
+            <h5></h5>
           </div>
           <div class="ibox-content">
-            <el-table :data="warnList" border style="width: 100%" :default-sort="{prop: ['IDCode', 'WELL_ID'], order: ['ascending', 'descending']}">
-              <el-table-column prop="RecordeID" label="ID" sortable></el-table-column>
-              <el-table-column prop="WELL_ID" label="井号" sortable></el-table-column>
-              <el-table-column prop="ActionTime" label="报警开始时间" ></el-table-column>
-              <el-table-column prop="IDCode" label="报警信息" sortable></el-table-column>
-              <el-table-column prop="ErrorEndTime" label="报警结束时间"></el-table-column>
+            <el-table :data="warnList" border style="width: 100%"
+                      :default-sort="{prop: ['IDCode', 'WELL_ID'], order: ['ascending', 'descending']}">
+              <el-table-column prop="RecordeID" label="ID" sortable width="90"></el-table-column>
+              <el-table-column prop="WELL_ID" label="井号" sortable width="90"></el-table-column>
+              <el-table-column prop="ActionTime" label="报警开始时间" width="180"></el-table-column>
+              <el-table-column prop="IDCode" label="报警信息" sortable width="180"></el-table-column>
+              <el-table-column prop="  ErrorEndTime" label="报警结束时间" width="180"></el-table-column>
               <el-table-column prop="isCheck" label="报警状态记录"></el-table-column>
             </el-table>
+            <div class="page">
+              <el-pagination layout="prev, pager, next" :total="1000" v-bind:current-Page="pageIndex"
+                             v-bind:page-size="pageSize" v-on:current-change="pageIndexChange"></el-pagination>
+            </div>
           </div>
         </div>
       </div>
@@ -28,32 +33,45 @@
   import 'datatables.net-bs/css/dataTables.bootstrap.css'
   import API from '../../../config/request'
   export default {
-      data () {
-        return {
-          warnList: []
-        }
+    data () {
+      return {
+        warnList: [],
+        pageIndex: 0,
+        pageSize: 15,
+        total: 0
+      }
+    },
+    created () {
+      this.pageRequest(this.pageIndex);
+    },
+    mounted () {
+
+    },
+    methods: {
+      pageIndexChange (pageIndex) {
+        this.pageIndex = this.pageIndex;
+        this.pageRequest(pageIndex);
       },
-      created () {
-          let body = {
-            blockid: 0,
-            size: 2
-          };
-          let that = this;
+      pageRequest (pageIndex) {
+        let body = {
+          blockid: 0,
+          size: this.pageSize,
+          page: pageIndex
+        };
+        let that = this;
         this.$http.post(API.wellLog, body).then(
           function (res) {
-            if (res.data.status === '0')
-            {
+            if (res.data.status === '0') {
               that.warnList = res.data.data;
             }
           });
-      },
-    mounted () {
-//      $('.dataTables-example').DataTable({
-//        dom: '<"html5buttons"B>lTfgitp'
-//      });
+      }
     }
   }
 </script>
 
 <style lang="less" rel="stylesheet/less">
+  .page {
+    float: right;
+  }
 </style>
