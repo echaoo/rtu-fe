9e879c5657f1645fa5837e53b4556ce9216fba2d
sky-ADMIN <546485299@qq.com>
2017-03-04 15:21:37 +0800

    update side bar and top bar

--- src/components/main/SideBar.vue
+++ src/components/main/SideBar.vue
@@ -3,14 +3,14 @@
 
     <div class="logo-area">
       <img src="../../assets/main/CXSYIcon.png" alt="">
-      <span>创新石油</span>
+      <span v-show="isSideBarOpen">创新石油</span>
     </div>
 
-    <el-menu theme="dark" default-active="0" class="menu" @select="handleClick" :router=true>
-      <el-menu-item index="map" :route="{name: 'map'}">地图</el-menu-item>
-      <el-menu-item index="indicator" :route="{name: 'indicator'}">示功图</el-menu-item>
-      <el-menu-item index="warnlog" :route="{name: 'warnlog'}">报警信息</el-menu-item>
-      <el-menu-item index="welllist" :route="{name: 'welllist'}">井列表</el-menu-item>
+    <el-menu theme="dark" :default-active="activeIndex" class="menu" @select="handleClick" :router=true>
+      <el-menu-item index="map" :route="{name: 'map'}"><i class="el-icon-picture"></i><span v-show="isSideBarOpen">地图</span></el-menu-item>
+      <el-menu-item index="indicator" :route="{name: 'indicator'}"><i class="el-icon-setting"></i><span v-show="isSideBarOpen">示功图</span></el-menu-item>
+      <el-menu-item index="warnlog" :route="{name: 'warnlog'}"><i class="el-icon-warning"></i><span v-show="isSideBarOpen">报警信息</span></el-menu-item>
+      <el-menu-item index="welllist" :route="{name: 'welllist'}"><i class="el-icon-more"></i><span v-show="isSideBarOpen">井列表</span></el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -20,8 +20,16 @@
 
   export default {
     computed: {
+      isSideBarOpen() {
+        return this.$store.state.layout.isSideBarOpen;
+      },
       activeIndex() {
         return this.$route.name
+      }
+    },
+    methods: {
+      handleClick() {
+
       }
     }
   }
@@ -32,15 +40,36 @@
     position: absolute;
     top: 0;
     height: 100%;
-    /*width: 220px;*/
     display: inline-block;
     overflow: hidden;
 
+    .logo-area {
+      display: inline-flex;
+      align-items: center;
+      height: 60px;
+      float: left;
+      background-color: #324157;
+      width: 100%;
+
+      span {
+        font-size: 20px;
+        color: white;
+      }
+
+      img {
+        height: 60px;
+        padding: 10px;
+        float: left;
+        width: 60px;
+      }
+    }
+
     .menu {
+      display: inline-block;
       height: 100%;
-      /*width: 280px;*/
-      overflow: scroll;
+      width: 100%;
       padding-bottom: 60px;
+      border-radius: 0;
     }
   }
 
@@ -51,7 +80,6 @@
 
     100% {
       width: 70px;
-      /*display: block;*/
     }
   }
 
