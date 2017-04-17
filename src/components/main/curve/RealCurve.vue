de47a04f5f93c5a42a47ab85284d6078b4ca5359
echaoo <zmk181@gmail.com>
2017-04-17 11:03:17 +0800

    update

--- src/components/main/curve/RealCurve.vue
+++ src/components/main/curve/RealCurve.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
-    wqwqwqwe245656
+    <div class="head-title">
+      <span class="head-left">实时曲线</span>
+      <span class="head-right">
+        <span>选择曲线参数</span>
+        <span>
+          <select v-model="selected">
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+          </select>
+        </span>
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     data () {
-      return {
-
-      }
+      return {}
     }
   }
 </script>
+<style>
+  .head-title {
+    height: 60px;
+    padding: 15px 30px;
+    background-color: #fff;
+  }
+
+  .head-left {
+    font-size: 20px;
+  }
+
+  .head-right {
+    float: right;
+    font-size: 16px;
+  }
+</style>
